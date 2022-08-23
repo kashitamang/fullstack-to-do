bb64b1b301e649a141f1b0800b238dade4393cef
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-23 16:44:28 -0700

    able to add a new user to to the user table

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,28 +2,28 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const UserService = require('../lib/services/UserService');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
-  email: 'test@example.com',
+  email: 'test4@example.com',
   password: '123456',
 };
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
 
-//   // Create an "agent" that gives us the ability
-//   // to store cookies between requests in a test
-//   const agent = request.agent(app);
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
 
-//   // Create a user to sign in with
-//   const user = await UserService.create({ ...mockUser, ...userProps });
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
 
-//   // ...then sign in
-//   const { email } = user;
-//   await agent.post('/api/v1/users/sessions').send({ email, password });
-//   return [agent, user];
-// };
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -43,5 +43,14 @@ describe('backend-express-template routes', () => {
       email,
     });
   });
-  
+
+  it('#GET returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
+  });
 });
